20456510eb8ada8a7cd3ebd82d261f9ad9d9fde4
Patryk Milkiewicz <g00397265@atu.ie>
2022-12-15 20:19:14 +0000

    Copied previous springboot project that ran on java v19 into this one which is v17

---
+++ src/main/java/ie/atu/PassengerSpringboot/PassengerSpringbootApplication.java
@@ -0,0 +1,35 @@
+package ie.atu.PassengerSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@SpringBootApplication
+@RequestMapping(path="/api/passenger")
+public class PassengerSpringbootApplication {
+	PassengerService myService;
+
+	public PassengerSpringbootApplication(PassengerService myService) {
+		this.myService = myService;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerSpringbootApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Passenger> getPssengers(){
+		return myService.getPassengers();
+	}
+
+	@GetMapping("/{passengerID}")
+	public Passenger getPassenger(@PathVariable String passengerID){
+		return myService.getPassenger(passengerID);
+	}
+
+}
---
+++ src/test/java/ie/atu/PassengerSpringboot/PassengerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.PassengerSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
